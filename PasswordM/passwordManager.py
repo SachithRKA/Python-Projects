d761a955730f4fdd519938671de3e56175efaddd
SachithRKA <SachithRKA@gmail.com>
2024-01-11 22:40:52 -0500

    Password Generator with text file save
    1. Generate a password with the generate password button.
    2. Saves the user's email, password, and website in a text file.
    3. Make sure the user inputs are valid.

--- PasswordM/passwordManager.py
+++ PasswordM/passwordManager.py
@@ -1,5 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+password_list = [random.choice(letters) for char in range(nr_letters)]
+password_list += [random.choice(symbols) for char in range(nr_symbols)]
+password_list += [random.choice(numbers) for char in range(nr_numbers)]
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+def generate_password():
+    passw_entry.insert(0, password)
 
 def sent_To_File():
     website_input = website_entry.get()
@@ -47,12 +70,12 @@ website_entry.focus()
 
 em_user_entry = Entry(font="Arial", width=35)
 em_user_entry.grid(row=2, column=1, columnspan=2)
-em_user_entry.insert(0, "sachithRKA@gmail.com")
+em_user_entry.insert(0,"sachithRKA@gmail.com")
 
 passw_entry = Entry(font="Arial", width=21)
 passw_entry.grid(row=3, column=1)
 
-generateP = Button(text="Generate Password", font="Arial")
+generateP = Button(text="Generate Password", font="Arial", command=generate_password)
 generateP.grid(row=3, column=2)
 
 add= Button(text="Add", width=36, command=sent_To_File)
