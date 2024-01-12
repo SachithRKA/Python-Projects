9119d813c8a61e7ec62582f50180ab9729a2601d
SachithRKA <SachithRKA@gmail.com>
2024-01-11 22:59:20 -0500

    Update: password input section
    When the password input section is clicked, the input input in that section is copied.

--- PasswordM/passwordManager.py
+++ PasswordM/passwordManager.py
@@ -1,6 +1,7 @@
+import random
 from tkinter import *
 from tkinter import messagebox
-import random
+import pyperclip
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -15,6 +16,7 @@ def generate_password():
     random.shuffle(password_list)
     password = "".join(password_list)
     passw_entry.insert(0, password)
+    pyperclip.copy(password)
 
 def sent_To_File():
     website_input = website_entry.get()
