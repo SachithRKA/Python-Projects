68504865cfa501c5bf4263d9b6c470fb5efdc78e
SachithRKA <SachithRKA@gmail.com>
2024-01-11 22:48:53 -0500

    Error Fix
    1. Fixed error: fixed password to changing password with the button press.

--- PasswordM/passwordManager.py
+++ PasswordM/passwordManager.py
@@ -2,26 +2,18 @@ from tkinter import *
 from tkinter import messagebox
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
-
-password_list = []
-password_list = [random.choice(letters) for char in range(nr_letters)]
-password_list += [random.choice(symbols) for char in range(nr_symbols)]
-password_list += [random.choice(numbers) for char in range(nr_numbers)]
-
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
 def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = []
+    password_list = [random.choice(letters) for char in range(random.randint(8, 10))]
+    password_list += [random.choice(symbols) for char in range(random.randint(2, 4))]
+    password_list += [random.choice(numbers) for char in range(random.randint(2, 4))]
+
+    random.shuffle(password_list)
+    password = "".join(password_list)
     passw_entry.insert(0, password)
 
 def sent_To_File():
